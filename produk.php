11f50a0469f8807f0934b730f5ea7f1a2d06d41b
Suhendani <dani.fs163@gmail.com>
2021-09-16 12:45:56 +0700

    update produk.php

--- produk.php
+++ produk.php
@@ -6,8 +6,11 @@
 // Game
 
 class Produk{
-
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
 }
 
 $produk1 = new Produk();
-$produk2 = new Produk();
+var_dump($produk1);
