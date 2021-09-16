456690371548472506625a69b180cef22baa865d
Suhendani <dani.fs163@gmail.com>
2021-09-16 21:07:04 +0700

    main

--- produk.php
+++ produk.php
@@ -12,5 +12,20 @@ class Produk{
            $harga = 0;
 }
 
-$produk1 = new Produk();
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Enigma";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Mario";
+// var_dump($produk2->judul);
+
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = "50000";
+var_dump($produk3);
+
+echo "Komik : $produk3->penulis, $produk3->penerbit";
