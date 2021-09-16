ac400b01b6e07bfe2e8088402a2dcc2f0855cfa0
Suhendani <dani.fs163@gmail.com>
2021-09-16 22:39:39 +0700

    meneyelesaiakan latihan dari video 4

---
+++ constructor.php
@@ -0,0 +1,42 @@
+<?php
+
+
+// Jualan Produk
+// Komik
+// Game
+
+class Produk{
+    public $judul = "judul",
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+
+
+        public function __construct($judul = "Judul", $penulis = "penulis", $penerbit = "Penerbit", $harga = 0){
+            $this->judul = $judul;
+            $this->penulis = $penulis;
+            $this->penerbit = $penerbit;
+            $this->harga = $harga;
+        }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+
+
+}
+
+
+
+$produk1 = new Produk("Naruto", "Masashi Kisimoto", "Shonen Jump", 50000);
+$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", "250000");
+$produk3 = new Produk("Rainbow Six");
+
+echo "Komik :" . $produk1->getLabel();
+echo "<br>";
+echo "Game :" . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
+
+
+?>
